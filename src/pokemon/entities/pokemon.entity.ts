a2340133a6847554ffce7f07a54ef274e07658b0
ruminahuelwalter <ruminahuelwalter@gmail.com>
2025-04-20 21:33:22 -0300

    Crear esquemas y modelos

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,20 @@
-export class Pokemon {}
+import { Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+    // id: string // Mongo ya me lo da
+    @Prop({
+        unique: true,
+        index: true
+    })
+    name: string;
+
+
+    @Prop({
+        unique: true,
+        index: true
+    })
+    no: number;
+}
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon);

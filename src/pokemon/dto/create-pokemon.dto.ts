f6b00f07cd3c2975ca68b116614d66468a427617
ruminahuelwalter <ruminahuelwalter@gmail.com>
2025-04-21 23:18:26 -0300

    Post - recibir y validar la data, cambio en globalPrefix

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { Prop } from "@nestjs/mongoose";
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+}

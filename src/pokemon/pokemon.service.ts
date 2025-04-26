3ceeddcb3c92aff8d64053e07c59b1f903c1c5e2
ruminahuelwalter <ruminahuelwalter@gmail.com>
2025-04-25 23:03:04 -0300

    Actualizar Pokemon en base de datos

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -58,8 +58,16 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(term);
+    if ( updatePokemonDto.name ){
+      updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
+      
+     const updatePokemon = await pokemon.updateOne( updatePokemonDto , {new: true})
+    
+    return {updatePokemon};
+    }
+
   }
 
   remove(id: number) {

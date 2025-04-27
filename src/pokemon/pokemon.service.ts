6509a17c7b534e7c27223195ccf20547496c0dbb
ruminahuelwalter <ruminahuelwalter@gmail.com>
2025-04-26 23:01:16 -0300

    Validacion en update

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -62,10 +62,21 @@ export class PokemonService {
     const pokemon = await this.findOne(term);
     if ( updatePokemonDto.name ){
       updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
+      try{
+
+        const updatePokemon = await pokemon.updateOne( updatePokemonDto , {new: true})
+        return { ...pokemon.toJSON(),...updatePokemon };
+        
+    } catch (error) {
+      if ( error.code === 11000) {
+        throw new BadRequestException(`Pokemon exists in db ${ JSON.stringify(error.keyValue)}`)
+      }
+      console.log(error);
+      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`)
       
-     const updatePokemon = await pokemon.updateOne( updatePokemonDto , {new: true})
+    }
+
     
-    return {updatePokemon};
     }
 
   }
